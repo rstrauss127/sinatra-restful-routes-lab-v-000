81ecc45f8a963ae83a2d6afd11f4c3d1efdbc045
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-12 23:47:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181112225737) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "recipe_name"
+    t.string "recipe_ingredients"
+    t.string "recipe_cook_time"
+  end
 
 end
