f897e1d30959a273f3422f9e2c4ecfcc2434e96e
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-13 02:11:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
 
   delete "/recipes/:id/delete" do
     recipe = Recipe.find_by_id(params[:id])
-    recipe.destroy
+    recipe.delete
     redirect to '/recipes'
   end
 
