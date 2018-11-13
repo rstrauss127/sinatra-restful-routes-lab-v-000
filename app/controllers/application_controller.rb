f2057ea1894ccf30c92c464a2cdfcfcc21abf931
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-13 00:32:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,7 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/recipes/new' do
-    erb :new
-  end
 
-  post '/recipes' do
-    @recipe = Recipe.create(:recipe_name => params[:recipe_name], :recipe_ingredients => params[:recipe_ingredients], :recipe_cook_time => params[:recipe_cook_time])
-    redirect to "/recipes/#{@recipe.id}"
-  end
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
